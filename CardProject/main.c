20141db2748d9ce3f18fcc44dcfaa95581fcf29e
dasilv-k <78146974+dasilv-k@users.noreply.github.com>
2023-10-09 16:08:52 +0200

    Initial Commit

---
+++ CardProject/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CardProject
+//
+//  Created by Kevin on 09/10/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
